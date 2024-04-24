2971fbf35bba3979219e8f2ccc4a99380328501e
Ian Bravyn. <ianbravynsa@gmail.com>
2024-04-24 16:21:31 +0300

    removed dead code

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('play/', views.play_music, name='play_music'),
-    path('stop/', views.stop_music, name='stop_music'),
-
-]
