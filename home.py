e0536d8459ef4f7538326379d56f78fcce03fd53
Ian Bravyn. <ianbravynsa@gmail.com>
2024-07-18 11:44:35 +0300

    .

--- home.py
+++ home.py
@@ -6,3 +6,14 @@ app = Flask(__name__)
 def index():
     return "<h1> Welcome to Jihl </h1>"
 
+@app.route('/about')
+def about():
+    return "<h2> About section </h2>"
+
+@app.route('/products')
+def products():
+    return "<h3> Products section </h3>"
+
+@app.route('/services')
+def services():
+    return "<h3> Services section </h3>"
