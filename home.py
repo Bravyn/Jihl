bc7927af0417d2073ab8c58546cbe94ba001d99b
Ian Bravyn. <ianbravynsa@gmail.com>
2024-09-25 17:39:26 +0300

    .

--- home.py
+++ home.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/hello/<name>')
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/hello/<name>', methods = ['GET', 'POST'])
 def index(name = None):
+    if request.method == 'POST':
+        input_val = request.form.get('inputBox')
+        return render_template("hello.html", person = input_val)
     return render_template("hello.html", person = name)
+
+
 
 @app.route('/about')
 def about():
