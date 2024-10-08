e82a61740558ed7f912b8de73a46cd7a99e0f5a5
Ian Bravyn. <ianbravynsa@gmail.com>
2024-10-08 19:11:10 +0300

    .

--- home.py
+++ home.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request
-from taglines import taglines
+from taglines import taglines, image_list
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/hello/<name>', methods = ['GET', 'POST'])
-def index(tagline = None):
+def index(tagline = None, images = None):
     if request.method == 'POST':
         input_val = request.form.get('inputBox')
         return render_template("hello.html", person = input_val)
-    return render_template("hello.html", tagline = taglines)
+    return render_template("hello.html", tagline = taglines, image= image_list)
 
 
 
@@ -24,3 +24,7 @@ def products():
 @app.route('/services')
 def services():
     return "<h3> Services section </h3>"
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
--- taglines.py
+++ taglines.py
@@ -1,3 +1,6 @@
 taglines = ["By leveraging machine learning algorithms, we provide real-time threat\
                 detection and automated incident response, enabling organizations to proactively identify\
                 vulnerabilities and mitigate risks.", "START EXPLOITING MACHINE LEARNING TODAY"]
+
+
+image_list = ["im.jpg", "im2.jpg","im.jpg", "im2.jpg"]
